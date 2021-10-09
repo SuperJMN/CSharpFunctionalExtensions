bb5599abb8cc069688c607df0a3f92540af2be77
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-10-10 00:13:34 +0200

    Improved tests

--- CSharpFunctionalExtensions.Tests/ResultTests/Extensions/WithResultOfTAndETests.cs
+++ CSharpFunctionalExtensions.Tests/ResultTests/Extensions/WithResultOfTAndETests.cs
@@ -1,47 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
 {
     public class WithResultOfTAndETests
     {
-        [Theory]
-        [InlineData("af", "bs", "af")]
-        [InlineData("as", "bf", "bf")]
-        [InlineData("af", "bf", "af,bf")]
-        [InlineData("as", "bs", "asbs")]
-        public void Verify(string a, string b, string expected)
+        [Fact]
+        public void Success_should_combine()
         {
-            var ea = Emit(a);
-            var eb = Emit(b);
+            var a = Result.Success<int, string>(2);
+            var b = Result.Success<int, string>(3);
 
-            var ec = ea.With<string, ErrorList>(eb, (x, y) => Result.Success<string, ErrorList>(x + y), (e1, e2) => new ErrorList(e1.Concat(e2)));
+            var r = a.With(b, (x, y) => x + y, (e1, e2) => "both failed");
 
-            var r = ec.Handle(list => string.Join(",", list));
-            r.Should().Be(expected);
-        }
-
-        private Result<string, ErrorList> Emit(string token)
-        {
-            if (token.EndsWith("s"))
-            {
-                return Result.Success<string, ErrorList>(token);
-            }
-
-            return Result.Failure<string, ErrorList>(new ErrorList(token));
-        }
-
-        private class ErrorList : List<string>
-        {
-            public ErrorList(IEnumerable<string> items) : base(items)
-            {
-            }
-
-            public ErrorList(string item) : this(new[] { item })
-            {
-            }
+            r.IsSuccess.Should().BeTrue();
+            r.Value.Should().Be(5);
         }
 
         [Fact]
@@ -49,9 +22,35 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
         {
             var a = Result.Failure<int, string>("Error 1");
             var b = Result.Failure<int, string>("Error 2");
-            var r = a.With(b, (x, y) => x + y, (s, s1) => "combined");
+
+            var r = a.With(b, (x, y) => x + y, (e1, e2) => "both failed");
+
             r.IsFailure.Should().BeTrue();
-            r.Error.Should().Be("combined");
+            r.Error.Should().Be("both failed");
+        }
+
+        [Fact]
+        public void Failure_in_first_should_fail_with_first_message()
+        {
+            var a = Result.Failure<int, string>("Error 1");
+            var b = Result.Success<int, string>(2);
+
+            var r = a.With(b, (x, y) => x + y, (e1, e2) => "both failed");
+
+            r.IsFailure.Should().BeTrue();
+            r.Error.Should().Be("Error 1");
+        }
+
+        [Fact]
+        public void Failure_in_second_should_fail_with_second_message()
+        {
+            var a = Result.Success<int, string>(2);
+            var b = Result.Failure<int, string>("Error 2");
+
+            var r = a.With(b, (x, y) => x + y, (e1, e2) => "both failed");
+
+            r.IsFailure.Should().BeTrue();
+            r.Error.Should().Be("Error 2");
         }
     }
 }

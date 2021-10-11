8b0ba0155787e99ff04c66cbadd8c901b47f431b
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-10-11 10:58:27 +0200

    Another one

--- CSharpFunctionalExtensions/Result/Extensions/BindError.cs
+++ CSharpFunctionalExtensions/Result/Extensions/BindError.cs
@@ -5,25 +5,25 @@ namespace CSharpFunctionalExtensions
     public partial class ResultExtensions
     {
         public static Result<T, E2> BindError<T, E, E2>(this Result<T, E> self,
-            Func<E, Result<T, E2>> map)
+            Func<E, Result<T, E2>> func)
         {
             if (self.IsSuccess)
             {
                 return Result.Success<T, E2>(self.Value);
             }
 
-            return map(self.Error);
+            return func(self.Error);
         }
 
         public static Result<T> BindError<T>(this Result<T> self,
-            Func<string, Result<T>> map)
+            Func<string, Result<T>> func)
         {
             if (self.IsSuccess)
             {
                 return Result.Success(self.Value);
             }
 
-            return map(self.Error);
+            return func(self.Error);
         }
     }
 }
